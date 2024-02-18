f541e0c557e68ad2bdd8e26bd0f90db57b9a179d
James <bowajk@gmail.com>
2024-02-18 16:56:45 +0400

    Connect to DevNet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use solana_client::pubsub_client::PubsubClient;
+use solana_client::rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter};
+use solana_sdk::commitment_config::CommitmentConfig;
+
 fn main() {
-    println!("Welcome to Reel");
+    let ws_url = String::from("wss://api.devnet.solana.com/");
+    let filter = RpcTransactionLogsFilter::All;
+    let config = RpcTransactionLogsConfig {
+        commitment: Some(CommitmentConfig::finalized()),
+    };
+
+    if let Ok(subscription) = PubsubClient::logs_subscribe(&ws_url, filter, config) {
+        let (mut ws_client, receiver) = subscription;
+
+        std::thread::spawn(move || loop {
+            match receiver.recv() {
+                Ok(logs) => {
+                    println!("Transaction executed in slot: {}", logs.context.slot);
+                    println!("  Signature: {}", logs.value.signature);
+                    println!(
+                        "  Status: {}",
+                        logs.value
+                            .err
+                            .map(|err| err.to_string())
+                            .unwrap_or_else(|| "Ok".to_string())
+                    );
+                    println!("  Log Messages:");
+                    for log in logs.value.logs {
+                        println!("    {log}");
+                    }
+                }
+                Err(err) => {
+                    println!("disconnected {:}", err);
+                    break;
+                }
+            }
+        })
+        .join()
+        .unwrap();
+        ws_client.shutdown().unwrap();
+    } else {
+        println!("could not connect to subscription service");
+    }
 }
